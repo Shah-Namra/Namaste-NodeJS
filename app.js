534cbc4cb8698230883488f729443738f1b3106b
Shah-Namra <21it106@svitvasad.ac.in>
2024-10-01 08:29:16 +0530

    Lecture 3

---
+++ app.js
@@ -0,0 +1,8 @@
+// var name ="Namra Shah";
+// var a = 10, b = 20;
+// console.log(name);
+// console.log(a+b);
+// console.log(this) // empty object   
+// console.log(global);
+console.log(globalThis === global);
+//console.log(__dirname);
